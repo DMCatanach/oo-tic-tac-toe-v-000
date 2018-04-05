f701d5a597578ea854296ea88c7b6f409a37368b
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-05 03:46:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,15 @@ class TicTacToe
   end
 
   def turn
-    #code
+    puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end 
   end
 
   def won?
